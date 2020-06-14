f6234aab1ebdb145e2099944a8a9066353c601b5
Thomas James <42183357+ThomasJames@users.noreply.github.com>
2020-06-14 22:50:29 +0100

    Feat: Mass Commit

--- VGG-16/VGG-16_Transfer_Learning.py
+++ VGG-16/VGG-16_Transfer_Learning.py
@@ -26,8 +26,10 @@ transform = transforms.Compose([
 ])
 
 # Load in the data
+
 trainset = ""
 testset = ""
+
 
 # Ask PyTorch for the pre-trained VGG-16 model
 vgg16 = models.vgg16(pretrained=True)
@@ -55,7 +57,52 @@ vgg16.classifier[-1] = nn.Sequential(
 print(vgg16.features)
 print(vgg16.classifier)
 
-# Change the criterion to nn.NLLoss
-criterion = nn.NLLLoss()
+# Change the criterion to Sigmoid, for binary classification tasks
+criterion = nn.Sigmoid()
+
+num_epochs = 1   # Define the number of iterations
+batch_loss = 0
+cum_epoch_loss = 0  # Sets a tracker for loss
+
+
+
+
+
+
+"""""
+Running data through the network
+"""""
+
+model = vgg16.to(device)
+optimiser = Adam(vgg16.parameters(()))
+
+for e in range(num_epochs):    # for each predefined epoch
+  cum_epoch_loss = 0
+  for batch, (images, labels) in enumerate(trainloader, 1):
+    images = images.to(device)  # Load the images to the device
+    labels = labels.to(device)  # Load the labels to the device
+
+    optimizer.zero_grad() # Zero out the gradients at each iter
+    logps = vgg16(images)  # Run the batch through the VGG-16 model, to see what predictions the model will provide
+    loss = criterion(logps, labels) # Calculate the loss
+    loss.backward() # Run the backwards pass
+    optimiser.step() # Update the weights using the loss values
+
+    batch_loss += loss.item()
+    print(f"Epoch({e}/{num_ephocs} : number ({batch}/{len(trainloader)}))) Batch loss : {loss.item()}")
+
+    print(f"Training loss : {batch_loss/len(trainloader)}")
+
+
+"""
+Evaluate the mode
+"""
+
+vgg16.to("cpu")
+vgg16.eval()
+
+with torch.no_grad():
+
+
 
 
